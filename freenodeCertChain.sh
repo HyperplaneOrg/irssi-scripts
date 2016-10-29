710b6ce8a1add0217e555d0096a7805601ae8a73
Andrew Michaelis <amac@hyperplane.org>
2016-10-29 15:01:43 -0700

    add freenode cert bulid

---
+++ freenodeCertChain.sh
@@ -0,0 +1,44 @@
+#!/bin/sh 
+
+# Oct 2016 - This shell script will build the certificate chain for 
+# freenode.net so your irssi client can verify the ssl connection. 
+# The docs on the freenode site https://freenode.net/kb/answer/chat
+# did not exactly work for me so I've posted this script to make the 
+# process more convenient. You should have an entry in 
+# your ~/.irssi/config that looks something like:
+#
+#  { 
+#    address = "chat.freenode.net";   
+#    chatnet = "freenode";    
+#    port = "6697";
+#    use_ssl = "yes";
+#    ssl_verify = "yes";
+#    ssl_cafile = "~/.irssi/freenodechain.pem";
+#  }
+#
+# This script will build the freenodechain.pem file for youe. It is 
+# assumed that you have curl and a somewhat modern version of openssl 
+# in your PATH; most MacOS and Linux systems have these as standard 
+# packages. You will also need the standard unix/shell program 
+# basename.
+
+GANDICERT=https://www.gandi.net/static/CAs/GandiStandardSSLCA2.pem 
+USRTRUST=http://crt.usertrust.com/USERTrustRSAAddTrustCA.crt 
+ADDTRUST=http://www.tbs-x509.com/AddTrustExternalCARoot.crt
+FOUT=freenodechain.pem
+
+function add_cert () {
+   C=$1
+   TP=$2
+   if [ ! -z $TP ]; then
+      curl $C | openssl x509 -inform der -outform pem >> $FOUT
+   else
+      curl $C >> $FOUT
+   fi
+}
+   
+echo -n "" > $FOUT
+add_cert $GANDICERT
+add_cert $USRTRUST 1
+add_cert $ADDTRUST 
+
